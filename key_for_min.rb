c0f3d5989dea89a7b1cf7a9100f411b497b1bbf7
luis leon <leonluis380@yahoo.com>
2021-06-02 13:36:54 -0400

    commit tree c0c1bad7c16347a910a6fe3112f998001f3ec6a3

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  lkey = nil
+  lnum = nil
+  hash.each do |key, value|
+    if lnum == nil || value < lnum
+        lkey = key
+        lnum = value
+        end
+    end
+    return lkey
 end
